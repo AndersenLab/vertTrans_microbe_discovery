1d1fc19288fd6c47617ec1587b18ed57e07caa94
lanceman999 <loconn13@jh.edu>
2025-10-24 14:27:41 -0400

    updated script

--- scripts/c_elegans/heatmap.R
+++ scripts/c_elegans/heatmap.R
@@ -1,2 +1,106 @@
 library(dplyr)
 library(ggplot2)
+install.packages("ggtext")
+library(ggtext)
+
+genera <- c("Nematocida","Homo","Mus", "Pseudomonas","Brucella") # Escherichia is not in the 15 highest % minimizers for any species
+
+control_ni <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/pathogen_unalignedBAM_SDSU/processed_data/c_elegans/Kraken2/microsporidia_ctr/notInfected/EukPath/all_strains.tsv", col_names = c("strain","perc","x1","x2","x3","x4","x5","x6","genus")) %>%
+  dplyr::filter(x5 == "G") %>%
+  dplyr::distinct(strain) %>%
+  dplyr::mutate(perc = 0, genus = NA, species = "Control (not-infected)") 
+
+control_ni <- tidyr::expand_grid(
+  strain = control_ni_strains$strain,
+  genus  = genera
+) %>%
+  mutate(perc = 0, species = "Control (not-infected)")
+
+control_i <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/pathogen_unalignedBAM_SDSU/processed_data/c_elegans/Kraken2/microsporidia_ctr/infected/EukPath/all_strains.tsv", col_names = c("strain","perc","x1","x2","x3","x4","x5","x6","genus")) %>%
+  dplyr::filter(x5 == "G") %>%
+  dplyr::mutate(perc = ifelse(perc >= 10, perc, 0)) %>%
+  dplyr::filter(genus %in% genera) %>%
+  dplyr::select(strain,perc,genus) %>%
+  distinct() %>%
+  dplyr::mutate(species = "Control (infected)")  %>%
+  dplyr::arrange(desc(perc)) 
+
+
+ce <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/pathogen_unalignedBAM_SDSU/processed_data/c_elegans/Kraken2/wild_strains/analysis/c_elegans_Kraken2classification_10perc.fixed.tsv", col_names = c("strain","perc","x1","x2","x3","x4","x5","x6","genus")) %>%
+  dplyr::filter(x5 == "G") %>%
+  dplyr::filter(genus %in% genera) %>%
+  dplyr::select(strain,perc,genus) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(species = "C. elegans") %>%
+  dplyr::arrange(desc(perc))
+  
+ce_top15strains <- ce %>% dplyr::slice_head(n=15) %>% dplyr::pull(strain)
+
+ce <- ce %>% dplyr::filter(strain %in% ce_top15strains)
+  
+            
+cb <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/pathogen_unalignedBAM_SDSU/processed_data/c_briggsae/Kraken2/wild_strains/analysis/c_briggsae_Kraken2classification_10perc.fixed.txt", col_names = c("strain","perc","x1","x2","x3","x4","x5","x6","genus")) %>%
+  dplyr::filter(x5 == "G") %>%
+  dplyr::filter(genus %in% genera) %>%
+  dplyr::select(strain,perc,genus) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(species = "C. briggsae")  %>%
+  dplyr::arrange(desc(perc))
+
+cb_top15strains <- cb %>% dplyr::slice_head(n=15) %>% dplyr::pull(strain)
+
+cb <- cb %>% dplyr::filter(strain %in% cb_top15strains)
+
+ct <- readr::read_tsv("/vast/eande106/projects/Lance/THESIS_WORK/pathogen_unalignedBAM_SDSU/processed_data/c_tropicalis/Kraken2/wild_strains/analysis/c_tropicalis_Kraken2classification_10perc.fixed.txt", col_names = c("strain","perc","x1","x2","x3","x4","x5","x6","genus")) %>%
+  dplyr::filter(x5 == "G") %>%
+  dplyr::filter(genus %in% genera) %>%
+  dplyr::select(strain,perc,genus) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(species = "C. tropicalis")  %>%
+  dplyr::arrange(desc(perc))
+
+ct_top15strains <- ct %>% dplyr::slice_head(n=15) %>% dplyr::pull(strain)
+
+ct <- ct %>% dplyr::filter(strain %in% ct_top15strains)
+
+
+all <- bind_rows(control_ni, control_i, ce, cb, ct) %>%
+  dplyr::group_by(species, strain) %>%
+  tidyr::complete(genus = genera, fill = list(perc = 0)) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(genus = factor(genus, levels = rev(genera))) %>%
+  dplyr::mutate(species_lab = dplyr::recode(species,
+      "C. elegans"   = "<i>C. elegans</i>",
+      "C. briggsae"  = "<i>C. briggsae</i>",
+      "C. tropicalis"= "<i>C. tropicalis</i>",
+      "CTR: Inf"     = "Control (infected)",
+      "CTR: not-Inf" = "Control (not-infected)"))
+
+heatmap <- ggplot(all, aes(x = strain, y = genus, fill = perc)) +
+  geom_tile(color = "white", linewidth = 0.15) +
+  facet_wrap(~ species_lab, scales = "free_x", nrow = 1) +
+  scale_fill_gradient(low = "skyblue", high = "red", name = "Percent minimizers", limits = c(0, 100)) +
+  labs(x = NULL, y = NULL) +
+  scale_y_discrete(expand = c(0,0)) +
+  theme(
+    panel.grid = element_blank(),
+    panel.border = element_rect(fill = NA, color = 'black'),
+    legend.title = element_text(size = 16, color = 'black'), 
+    legend.text  = element_text(size = 14),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, color = 'black'),
+    # strip.text = element_text(face = "bold.italic", size = 16),
+    strip.text = ggtext::element_markdown(face = "bold", size = 15),  
+    panel.spacing.x = unit(0.2, "lines"),
+    axis.text.y = element_text(size = 14, face = 'bold.italic', color = 'black'),
+    legend.position = "right")
+heatmap
+
+
+
+
+
+
+
+
+
+
